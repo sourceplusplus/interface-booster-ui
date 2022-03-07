62ff6d3980848580d85762408bd8ee81ae141462
Brandon Fergerson <bfergerson@apache.org>
2022-03-07 09:40:39 +0100

    try

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.diffplug.spotless")
     id("org.jetbrains.kotlin.jvm")
+    id("com.github.node-gradle.node")
 }
 
 val platformGroup: String by project
@@ -20,4 +21,20 @@ dependencies {
     implementation("io.vertx:vertx-web:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+}
+
+tasks {
+    register<com.github.gradle.node.npm.task.NpmTask>("buildSkyWalkingUI") {
+        dependsOn("npmInstall")
+        npmCommand.set(listOf("run", "build"))
+        node.nodeProjectDir.set(file("./src/main/skywalking-booster-ui"))
+    }
+    register<Copy>("moveSkyWalkingUI") {
+        shouldRunAfter("buildSkyWalkingUI")
+        from(file("./src/main/skywalking-booster-ui/lib"))
+        into(file("./src/main/resources/webroot"))
+    }
+    getByName("build") {
+        dependsOn("buildSkyWalkingUI", "moveSkyWalkingUI")
+    }
 }
