563539b90a8766b3a563c9b7a77b72b61df57dd3
Brandon Fergerson <bfergerson@apache.org>
2022-03-10 12:59:38 +0100

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,7 @@ dependencies {
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("com.github.sourceplusplus.interface-portal:portal-jvm:4cd858772b") { isTransitive = false }
-    implementation("com.github.sourceplusplus.protocol:protocol:e540bbcccb")
+    implementation("com.github.sourceplusplus.protocol:protocol:a2cd759830")
 }
 
 tasks {
